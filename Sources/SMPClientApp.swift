b1688e4f293768edb7483a670866fbe190175f0e
Andy Stechishin <andy.stechishin@gmail.com>
2024-05-16 13:10:12 -0600

    Initial commit

---
+++ Sources/SMPClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMPClientApp.swift
+//  SMPClient
+//
+//  Created by Andy Stechishin on 2024-05-16.
+//
+
+import SwiftUI
+
+@main
+struct SMPClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
